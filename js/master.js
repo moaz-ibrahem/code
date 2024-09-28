6a4fc6be2e1ba6a93f046991dee3b191eaae36f6
moaz ibrahem <moazibrahem952@gmail.com>
2024-09-28 20:24:14 +0300

    we code

---
+++ js/master.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed('.auto_type', {
+    strings: ["innovate","revolutionize", "ideate", "develop", "transform","code"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    looped : true
+});
